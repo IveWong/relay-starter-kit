adac1bdcdf9c79044db34f7ad4a45fe258b07cd9
Ive Wang <Ive@Ives-MacBook-Pro.local>
2015-09-25 18:11:32 +0800

    add offical-tuorial game example

--- js/routes/AppHomeRoute.js
+++ js/routes/AppHomeRoute.js
@@ -1,10 +1,21 @@
+// export default class extends Relay.Route {
+//   static queries = {
+//     viewer: () => Relay.QL`
+//       query {
+//         viewer
+//       }
+//     `,
+//   };
+//   static routeName = 'AppHomeRoute';
+// }
+
+
+// Tutorial - Game
+// ================================ start
 export default class extends Relay.Route {
+  static path = '/';
   static queries = {
-    viewer: () => Relay.QL`
-      query {
-        viewer
-      }
-    `,
+    game: () => Relay.QL`query { game }`,
   };
   static routeName = 'AppHomeRoute';
 }
